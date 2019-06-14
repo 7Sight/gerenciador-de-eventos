94597de7e46105610649e487129cf1f2aab8eb17
Guilherme Peixoto <guilhermepeixoto27@gmail.com>
2019-06-14 16:30:26 -0300

    merge Guilherme e rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#login'
-
-  get 'index', to: 'pages#index'
+  get 'cadastrarEvento', to: 'pages#registerEvent'
+  get 'eventos', to: 'pages#index'
 
 end
